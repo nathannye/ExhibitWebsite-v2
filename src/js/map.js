8427707ed95717a1345cc0a9c1733bfef8d125f5
nathannye <79227199+nathannye@users.noreply.github.com>
2022-03-29 01:43:24 -0400

    map kill marker

--- src/js/map.js
+++ src/js/map.js
@@ -1,30 +1,32 @@
 import mapboxgl from "mapbox-gl";
 
 let container = document.getElementById("galleryMap");
-// let markerAdd = document.getElementById("mapMarker");
 
-mapboxgl.accessToken =
-  "pk.eyJ1IjoibnllLTExIiwiYSI6ImNsMG9zeGRuOTE4MHMza3Rrcm5qcjVoZzgifQ.gB3lktc24CQYcAauqQ40Fw";
-var map = new mapboxgl.Map({
-  container: container,
-  center: [-85.47859669797005, 43.68930899669416],
-  pitch: 35,
-  zoom: 16,
-  style: "mapbox://styles/nye-11/cl0oszoxn000d14teqidfpqqn",
-});
+window.onload = () => {
+  mapboxgl.accessToken =
+    "pk.eyJ1IjoibnllLTExIiwiYSI6ImNsMG9zeGRuOTE4MHMza3Rrcm5qcjVoZzgifQ.gB3lktc24CQYcAauqQ40Fw";
+  const map = new mapboxgl.Map({
+    container: container,
+    center: [-85.47859669797005, 43.68930899669416],
+    pitch: 35,
+    zoom: 16,
+    style: "mapbox://styles/nye-11/cl0oszoxn000d14teqidfpqqn",
+  });
 
-const attr = new mapboxgl.AttributionControl({
-  customAttribution: "",
-});
+  // const attr = new mapboxgl.AttributionControl({
+  //   customAttribution: "",
+  // });
 
-const nav = new mapboxgl.NavigationControl({
-  anchor: "bottom-right",
-  showCompass: false,
-});
+  // const nav = new mapboxgl.NavigationControl({
+  //   anchor: "bottom-right",
+  //   showCompass: false,
+  // });
 
-map.addControl(nav, "bottom-right");
-// map.setLngLat([-85.47859669797005, 43.68930899669416]);
-map.addTo(map);
+  // map
+  //   .addControl(nav, "bottom-right")
+  //   .setLngLat([-85.47859669797005, 43.68930899669416]);
+  // // .addTo(map);
+};
 
 // const marker = new mapboxgl.Marker()
 //   .setLngLat([-85.47859669797005, 43.68930899669416])
